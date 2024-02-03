2150adbac0b69acb1408f05dc03b093f2c392be5
Fsoky <cyberuest0x12@gmail.com>
2024-02-04 02:37:27 +0600

    Config

---
+++ src/config_reader.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE_LOCATION = Path(".env").resolve()
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: SecretStr
+    DATABASE_URL: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_LOCATION,
+        env_file_encoding="utf-8"
+    )
+
+
+config = Settings()
